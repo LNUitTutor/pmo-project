eb07604d33616235220d72dc25029727b4aae26d
julia802 <58184818+julia802@users.noreply.github.com>
2019-11-26 13:55:24 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import numpy as np
 #TODO:task2, обчислити скалярний добуток (Vi,Vj)
 
 def FindScalarProduct(a,b): # функція, що шукає скалярний добуток
-    product_arr = np.polymul(a,b) # множимо два поліноми 
+    product_arr = np.polymul(a,b) # перемножуємо 
     poduct_poly = np.poly1d(product_arr) # перетворюємо результат на поліном
     integral_poly = np.polyint(poduct_poly) # знаходимо інтеграл
     
