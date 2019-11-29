7e3380c06c09d3824454319d027214913e015861
julia802 <58184818+julia802@users.noreply.github.com>
2019-11-29 17:07:54 +0200

    Add files via upload

---
+++ scalar_product.py
@@ -0,0 +1,4 @@
+def scalar_product(a_poly:np.ndarray, b_poly:np.ndarray)->float: 
+    """Find scalar product of two polynomials"""
+    integral = np.polyint(np.polymul(a_poly, b_poly)) # множимо поліноми і знаходимо первісну
+    return integral(1) - integral(0) # від інтегралу в точці 1 - інтеграл в точці 0
